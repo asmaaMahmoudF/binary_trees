068d9d6b31fafbf1d4c636d9dc8388e6b087c75b
vulturepremier <vulturepremier@gmail.com>
2024-06-05 18:49:03 +0300

    Sometimes Simplicity is very Complex

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - A function to Delete an Entire tree
+ * @tree: A pointer which points to the root of the tree
+ * Return: Nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *ptr;
+
+	ptr = tree;
+	if (tree == NULL)
+		return;
+	else
+	{
+		if (ptr != NULL)
+		{
+			binary_tree_delete(ptr->left);
+			binary_tree_delete(ptr->right);
+		}
+		free(ptr);
+	}
+}
